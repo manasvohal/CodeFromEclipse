bb9485bfdd9e2af4c012449fa4303c10185e0ee6
Manas Vohal <manas.vohal@outlook.com>
2021-03-19 18:01:14 -0400

    Selection Sort version 2

--- JavaIO/src/SelectionSort.java
+++ JavaIO/src/SelectionSort.java
@@ -6,6 +6,8 @@ public class SelectionSort {
 		Scanner reader = new Scanner(System.in);
 		int currentNumber = 0;
 		System.out.println("How many elements do you want the array to contain");
+		System.out.println("Do you want to put it in asscending or desscending order");
+		String answer = reader.nextLine();
 		int size = reader.nextInt();
 		int[] numbers = new int[size];
 		
@@ -14,16 +16,28 @@ public class SelectionSort {
 			numbers[i] = reader.nextInt();
 		}
 		System.out.println(Arrays.toString(numbers));
-		
-		for(int i = 0; i < numbers.length-1; i++) {
-			if(numbers[i] > numbers[i+1]) {
-				currentNumber = numbers[i];
-				numbers[i] = numbers[i+1];
-				numbers[i+1] = currentNumber;
-				i = -1;
-			}
-		}
-		System.out.println(Arrays.toString(numbers));
+//		if(answer.equals("a")) {
+//			for(int i = 0; i < numbers.length-1; i++) {
+//				if(numbers[i] > numbers[i+1]) {
+//					currentNumber = numbers[i];
+//					numbers[i] = numbers[i+1];
+//					numbers[i+1] = currentNumber;
+//					i = -1;
+//				}
+//			}
+//		}
+//		else if(answer.equals("d")) {
+//			for(int i = 0; i < numbers.length-1; i++) {
+//				if(numbers[i] < numbers[i+1]) {
+//					currentNumber = numbers[i+1];
+//					numbers[i+1] = numbers[i];
+//					numbers[i] = currentNumber;
+//					i = -1;
+//				}
+//			}
+//		}
+//		
+//		System.out.println(Arrays.toString(numbers));
 		
 	}
 
